81dcfa6d6e3b4132492580a24031be46fa7033a3
Vansh <vanshkashyap.003@gmail.com>
2026-03-27 21:33:13 +0530

    Removed large model file

--- main.py
+++ main.py
@@ -38,9 +38,13 @@ if not os.path.exists(MODEL_FILE):
     housing = pd.read_csv('housing.csv')
 
     # 2. Creating a stratified dataset
-    housing['income_cat'] = pd.cut(housing['median_income'],
-        bins=[0, 1.5, 3.0, 4.5, 6.0, np.inf],
-        labels=[1, 2, 3, 4, 5])
+    housing.columns = housing.columns.str.strip()
+
+    housing['income_cat'] = pd.cut(
+        housing['median_income'],
+        bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+        labels=[1, 2, 3, 4, 5]
+    )
 
     # 3. Spliting the dataset into train and test sets
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
